aa218185c6b84f1bab263a8138b9bb0eb299a3c6
onurc4kir <onurc4kir@gmail.com>
2023-06-23 00:18:08 +0300

    json ignore fix

--- src/main/java/dev/onurcakir/ecommerce/model/User.java
+++ src/main/java/dev/onurcakir/ecommerce/model/User.java
@@ -1,6 +1,7 @@
 package dev.onurcakir.ecommerce.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
